79c1c7293a82213e1051d450e4d9b5ee6f31a54b
Archana90663 <adhyani@wisc.edu>
2022-01-27 11:49:39 -0500

    #2 Created simple startpage

--- new_pastebin_frontend/src/app/app-routing.module.ts
+++ new_pastebin_frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SubmitpageComponent } from './submitpage/submitpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'submitpage', component: SubmitpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[
+  HomepageComponent,
+  SubmitpageComponent
+];
--- new_pastebin_frontend/src/app/app.module.ts
+++ new_pastebin_frontend/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SubmitpageComponent } from './submitpage/submitpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    NavbarComponent,
+    SubmitpageComponent
   ],
   imports: [
     BrowserModule,
