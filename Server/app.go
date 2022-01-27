df0cbdaa9328b2ccdf38443e16cc97145181615b
Param Gupta <param.gupta107@gmail.com>
2022-01-27 18:47:26 -0500

    #3 added route to post text

--- Server/app.go
+++ Server/app.go
@@ -21,13 +21,13 @@ type App struct {
 }
 
 func (a *App) start() {
-	a.db.AutoMigrate(&Person{})
+	a.db.AutoMigrate(&Person{}, &Text{})
 
 	// Add test data into db
 	a.db.Create(&Person{Id: 1, FirstName: "fn", LastName: "ln"})
 
 	a.r.HandleFunc("/test", a.test).Methods("GET")
-
+	a.r.HandleFunc("/api/submitText", a.addText).Methods("POST")
 	webapp, err := fs.Sub(static, "static")
 	if err != nil {
 		fmt.Println(err)
@@ -56,7 +56,21 @@ func (a *App) test(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Write(jsonResponse)
 }
-
+func (a *App) addText(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var text Text
+	err := json.NewDecoder(r.Body).Decode(&text)
+	if err != nil {
+		sendErr(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	err = postText(a.db, text)
+	if err != nil {
+		sendErr(w, http.StatusInternalServerError, err.Error())
+	} else {
+		w.WriteHeader(http.StatusCreated)
+	}
+}
 func sendErr(w http.ResponseWriter, code int, message string) {
 	resp, _ := json.Marshal(map[string]string{"error": message})
 	http.Error(w, string(resp), code)
