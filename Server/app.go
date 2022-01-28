cfbd2b099e948efff1ac1e74ba0258a7f5c4f045
Param Gupta <param.gupta107@gmail.com>
2022-01-27 19:00:48 -0500

    #3 verify request format

--- Server/app.go
+++ Server/app.go
@@ -64,6 +64,9 @@ func (a *App) addText(w http.ResponseWriter, r *http.Request) {
 		sendErr(w, http.StatusBadRequest, err.Error())
 		return
 	}
+	if text.Body == "" {
+		sendErr(w, http.StatusInternalServerError, "Cannot post text with empty body")
+	}
 	err = postText(a.db, text)
 	if err != nil {
 		sendErr(w, http.StatusInternalServerError, err.Error())
