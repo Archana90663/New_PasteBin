02a8bce6fdc4eb41828c011cd2c9b6a1769f973f
Param Gupta <param.gupta107@gmail.com>
2022-02-04 06:14:52 -0500

    Submit text #61

--- new_pastebin_frontend/src/app/homepage/homepage.component.ts
+++ new_pastebin_frontend/src/app/homepage/homepage.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Paste } from './pastestype';
+import { Paste } from '../types/pastestype';
 
 @Component({
   selector: 'app-homepage',
--- new_pastebin_frontend/src/app/submitpage/submitpage.component.ts
+++ new_pastebin_frontend/src/app/submitpage/submitpage.component.ts
@@ -1,5 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Paste } from '../types/pastestype';
+import { MatSnackBar } from "@angular/material/snack-bar";
+import {Router} from '@angular/router';
 
+interface SubmitTextPayload{
+  "title": string,
+  "body": string,
+  "expire_at"? : string
+}
 @Component({
   selector: 'app-submitpage',
   templateUrl: './submitpage.component.html',
@@ -7,9 +16,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubmitpageComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private httpClient: HttpClient,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) { }
+  textModel = new Paste("","","","","")
   ngOnInit(): void {
   }
-
+  postText(){
+  const payload:SubmitTextPayload = {"title":this.textModel.title,"body":this.textModel.body}
+  if(this.textModel.expire_at != ""){
+    payload.expire_at =  (new Date(Date.parse(this.textModel.expire_at))).toISOString()
+  }
+    this.httpClient.post<any>("http://localhost:8080/api/submitText", payload).subscribe(
+      response => {
+        this.showMessage("TEXT POSTED")
+        this.router.navigateByUrl('/textpage?id='+response.id);
+      },
+      error => {
+        if(error.status == 400){
+          this.showMessage(error.error.error)
+          
+        }
+      }
+    );
+    console.log(payload)
+  }
+  showMessage(message: string) {
+    this.snackBar.open(message, "OK");
+  }
 }
