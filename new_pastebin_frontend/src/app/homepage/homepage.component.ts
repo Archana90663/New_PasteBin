d2d92234641f8888aa908f1375db0cdd8ceade77
Param Gupta <param.gupta107@gmail.com>
2022-02-04 05:12:13 -0500

    Display all texts in cards #80

--- new_pastebin_frontend/src/app/homepage/homepage.component.ts
+++ new_pastebin_frontend/src/app/homepage/homepage.component.ts
@@ -26,5 +26,21 @@ export class HomepageComponent implements OnInit {
       }
     );
   }
+  getExpireIn(expire_at: string): string{
+    var secondsDiff = (Date.parse(expire_at) - (new Date()).getTime())/1000
+    if (secondsDiff > 86400)  { 
+      return Math.floor(secondsDiff/86400) + ' Days'
+     }
+     if (secondsDiff > 3600)  { 
+      return Math.floor(secondsDiff/3600) + ' Hours'
+     }
+     if (secondsDiff > 60)  { 
+      return Math.floor(secondsDiff/60) + ' Minutes'
+     }
+     if (secondsDiff > 0) { 
+      return secondsDiff + ' Seconds'
+     }
+    return "jj"
+  }
 
 }
