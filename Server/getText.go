95ec1ae5698afe3c0676cc4a01faa8a701e2f325
Param Gupta <param.gupta107@gmail.com>
2022-03-01 16:56:14 -0500

    Don't return user ip when fetching paste #149

--- Server/getText.go
+++ Server/getText.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"errors"
+	"time"
 
 	"gorm.io/gorm"
 )
 
+type TextView struct {
+	Id         string     `json:"id"`
+	Body       string     `json:"body"`
+	Title      string     `json:"title"`
+	CreatedAt  time.Time  `json:"created_at"`
+	Expire_at  *time.Time `json:"expire_at"`
+	TextLength int        `json:"TextLength"`
+	Tag        string     `json:"tag"`
+}
 type getTextRequest struct {
 	Id string `json:"id"`
 }
 
-func getText(db *gorm.DB, id string) []Text {
-	var text []Text
-	err := db.First(&text, "id=?", id).Error
+func getText(db *gorm.DB, id string) []TextView {
+	var text []TextView
+	err := db.Model(&Text{}).First(&text, "id=?", id).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil
 	} else if err != nil {
