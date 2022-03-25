6277cac6b00007f2ede555e22cd87fae2740ae62
Archana90663 <adhyani@wisc.edu>
2022-03-25 09:58:44 -0400

    #202 Displays the pastes created by the user who's logged in

--- new_pastebin_frontend/src/app/types/pastestype.ts
+++ new_pastebin_frontend/src/app/types/pastestype.ts
@@ -1,7 +1,7 @@
 export class Paste{
     constructor (
         public id: string,
-        public userID: string,
+        public userID: string, //changed it
         public title: string,
         public created_at: string,
         public expire_at: string,
