e1a48e13e8c2b2467deb8b67fa49ccc4e47ab2a9
unknown <bkojusner@ufl.edu>
2022-03-31 18:39:04 -0400

    #265 Check if user is logged in

--- new_pastebin_frontend/cypress/integration/loggedin_tag_update.js
+++ new_pastebin_frontend/cypress/integration/loggedin_tag_update.js
@@ -1,5 +1,9 @@
 describe('Tests Logged in User Paste Permissions', function () {
   it('Checks that a Logged in user has all paste permissions', function () {
-    
+    // Verify User is logged in
+    cy.visit('http://localhost:4200/loginpage')
+    cy.pause()
+    cy.get('button').contains("Continue With Google").should('not.exist')
+
   })
 })
