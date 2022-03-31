44016b78abd4ee792f5216aab39fed91cd9410d1
unknown <bkojusner@ufl.edu>
2022-03-31 16:15:49 -0400

    #264 Need to revisit Automatic Login for last part

--- new_pastebin_frontend/cypress/integration/loggedin_check.js
+++ new_pastebin_frontend/cypress/integration/loggedin_check.js
@@ -1,5 +1,17 @@
 describe('Checks if a user is logged in', function () {
     it('Checks Login Page', function () {
         cy.visit('http://localhost:4200/loginpage')
+
+        // Check the user is logged out
+        cy.get('button').contains("Continue With Google")
+
+        // Log the user in with test credentials
+
+        // Refresh page to update logged info
+        cy.visit('http://localhost:4200/loginpage')
+        cy.reload()
+
+        // Check the user is logged in
+        cy.get('button').contains("Continue With Google").should('not.exist')
     })
 })
