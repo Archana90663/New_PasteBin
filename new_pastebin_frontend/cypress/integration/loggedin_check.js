616ffcb26f3605893cb7e69d155f1adb70dcc81e
unknown <bkojusner@ufl.edu>
2022-03-31 17:38:43 -0400

    #264 Test to verify user is logged in

--- new_pastebin_frontend/cypress/integration/loggedin_check.js
+++ new_pastebin_frontend/cypress/integration/loggedin_check.js
@@ -2,10 +2,11 @@ describe('Checks if a user is logged in', function () {
     it('Checks Login Page', function () {
         cy.visit('http://localhost:4200/loginpage')
 
-        // Check the user is logged out
-        cy.get('button').contains("Continue With Google")
-
-        // Log the user in with test credentials
+        // Programatically Login to the app
+        // The workaround is to manually intervene using the Electron94 browser to login
+        // The page will redirect to the login page and then just verify if there is a button to continue with Google
+        // Should be cleaned up in the next sprint
+        cy.pause()
 
         // Refresh page to update logged info
         cy.visit('http://localhost:4200/loginpage')
