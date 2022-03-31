c26d27794c273b40ff774c9e661a23105b68469b
unknown <bkojusner@ufl.edu>
2022-03-31 19:55:39 -0400

    #268 Tests to see if logout is successful

--- new_pastebin_frontend/cypress/integration/logging_out_spec.js
+++ new_pastebin_frontend/cypress/integration/logging_out_spec.js
@@ -4,5 +4,12 @@ describe('Tests Non-Logged in User Paste Permissions', function () {
     cy.visit('http://localhost:4200/loginpage')
     cy.contains("Logout from Google")
 
+    // If button asks to Logout then user is logged in
+    cy.get('button').contains("Logout from Google").click()
+
+    // Verify we have logged out
+    cy.visit('http://localhost:4200/loginpage')
+    cy.refresh()
+    cy.contains("Logout from Google").should('not.exist')
   })
 })
