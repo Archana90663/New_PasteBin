9a231ebc32af8020cf782e60fa3630d2ce280f5a
unknown <bkojusner@ufl.edu>
2022-03-31 16:07:27 -0400

    #266 Fully verify Non-Logged in User can not make Private Pastes

--- new_pastebin_frontend/cypress/integration/non-loggedin_paste.js
+++ new_pastebin_frontend/cypress/integration/non-loggedin_paste.js
@@ -6,20 +6,9 @@ describe('Tests Non-Logged in User Paste Permissions', function () {
     // If button asks to Continue with Google then user is logged out
     cy.get('button').contains("Continue With Google")
 
-    /*/ Fill out the body of the paste
-    cy.get('#richtexteditor_1007234207_0').type('This is the body')
-
-    // Fill out the date of the paste
-    cy.get('#mat-input-1').type('2022-04-03T08:30')
-
-    // Select paste type
+    // Verify we do not have Private option available in pastes
+    cy.visit('http://localhost:4200/submitpage')
     cy.get('#mat-select-0').click()
-    cy.contains('Unlisted').click()
-
-    // Submit paste
-    cy.get('#submitButton').click()
-
-    // Refresh page to prove persistance
-    cy.reload()*/
+    cy.contains('Private').should('not.exist')
   })
 })
