aced52f421aa1d6ab9274dbbb48b3a8430cba60c
unknown <bkojusner@ufl.edu>
2022-03-31 20:49:55 -0400

    #206 Login Directly Calls Google API

--- new_pastebin_frontend/src/app/navbar/navbar.component.ts
+++ new_pastebin_frontend/src/app/navbar/navbar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { SocialAuthService} from 'angularx-social-login';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SocialAuthService, GoogleLoginProvider, SocialUser } from 'angularx-social-login';
+import { BehaviorSubject } from 'rxjs';
 
 
 @Component({
@@ -9,15 +13,17 @@ import { SocialAuthService} from 'angularx-social-login';
 })
 export class NavbarComponent implements OnInit {
 
-
+  public socialUser: SocialUser = new SocialUser;
   logged:boolean = false;
 
-
-  constructor(private auth: SocialAuthService) {
-   }
+  constructor(
+    private socialAuthService: SocialAuthService, 
+    private httpClient: HttpClient,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
-    this.auth.authState.subscribe(user =>{
+    this.socialAuthService.authState.subscribe(user =>{
       if(user != null){
         this.logged = true;
       }
@@ -27,5 +33,35 @@ export class NavbarComponent implements OnInit {
     });
 
   }
+
+  login() {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    this.socialAuthService.authState.subscribe(user =>{
+      this.socialUser = user;
+      console.log("ID: " + this.socialUser.id);
+
+      let headers = new HttpHeaders({'Content-Type': 'application/json'});
+    this.httpClient.post<any>("http://localhost:8080/api/signup", {"idToken": this.socialUser.idToken, "handle":"blah"}, {headers: headers, withCredentials: true}).subscribe(
+        res=>{
+          console.log("signedup: " + res);
+        }
+      );
+      this.httpClient.post<any>("http://localhost:8080/api/login", {"idToken": this.socialUser.idToken}, {headers: headers, withCredentials: true}).subscribe(
+        res=>{
+          console.log("logged: " + res);
+        }
+      );
+      this.httpClient.get<any>("http://localhost:8080/api/verifyLogin", {withCredentials:true}).subscribe(
+        response=>{
+          console.log("response: " + response.loggedIn);
+        }
+      );
+    });
+    
+    this.router.navigateByUrl('/');
+    console.log("ID: " + localStorage.getItem('userID'));
+  }
+
+ 
 
 }
