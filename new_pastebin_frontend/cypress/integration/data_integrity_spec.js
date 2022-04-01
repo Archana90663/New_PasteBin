e22c4d2d5a64a82b3ec63f0c6e209d8af873c20a
unknown <bkojusner@ufl.edu>
2022-04-01 19:18:20 -0400

    #219 Tests for Private/Unlisted pastes as well

--- new_pastebin_frontend/cypress/integration/data_integrity_spec.js
+++ new_pastebin_frontend/cypress/integration/data_integrity_spec.js
@@ -15,7 +15,18 @@ describe('Tests Paste Visibility Integrity', function () {
     })
 
     it('Tests Private Pastes', function () {
-        
+        // Create the paste
+        cy.visit('http://localhost:4200/submitpage')
+        cy.get('#mat-input-0').type('Unlisted Paste Integrity Test')
+        cy.get('#richtexteditor_1007234207_0').type('This is the body')
+        cy.get('#mat-input-1').type('2022-06-03T08:30')
+        cy.get('#mat-select-0').click()
+        cy.contains('Unlisted').click()
+        cy.get('#submitButton').click()
+
+        // Once paste created go to homepage
+        cy.visit('http://localhost:4200')
+        cy.contains('Unlisted Paste Integrity Test').should('not.exist')
     })
     
 })
