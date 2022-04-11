35a9d6596c6df790c9f1b3683364474169298076
unknown <bkojusner@ufl.edu>
2022-04-11 11:43:35 -0400

    #311 Signup page API connection progress

--- new_pastebin_frontend/src/app/signuppage/signuppage.component.ts
+++ new_pastebin_frontend/src/app/signuppage/signuppage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-signuppage',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignuppageComponent implements OnInit {
 
-  constructor() { }
+  signupForm!: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient,
+  ) { }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() { return this.signupForm.controls; }
+
+  public signup(): void{
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.signupForm.invalid) {
+        return;
+    }
+
+    this.loading = true;
+    
+    let headers = new HttpHeaders({'Content-Type': 'application/json'});
+    this.httpClient.post<any>("http://localhost:8080/api/signup", {"idToken": this.signupForm.value, "handle":"blah"}, {headers: headers, withCredentials: true}).subscribe(
+      res=>{
+        console.log("signedup: " + res);
+      }
+    );
   }
 
 }
