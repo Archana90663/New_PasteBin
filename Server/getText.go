d70c7a79edfd2dc77173be7d4a12f989df5b2945
Param Gupta <param.gupta107@gmail.com>
2022-04-17 05:47:55 -0400

    return paste author details #398

--- Server/getText.go
+++ Server/getText.go
@@ -8,7 +8,7 @@ import (
 )
 
 type TextView struct {
-	Id         string     `json:"id"`
+	Id         string     `gorm:"texts.id" json:"id"`
 	UserID     string     `json:"userID"`
 	Body       string     `json:"body"`
 	Title      string     `json:"title"`
@@ -17,6 +17,9 @@ type TextView struct {
 	TextLength int        `json:"TextLength"`
 	Tag        string     `json:"tag"`
 	Language   string     `json:"language"`
+	Name       string     `json:"name"`
+	Email      string     `json:"email"`
+	Picture    string     `json:"picture"`
 }
 type getTextRequest struct {
 	Id string `json:"id"`
@@ -24,7 +27,9 @@ type getTextRequest struct {
 
 func getText(db *gorm.DB, id string) []TextView {
 	var text []TextView
-	err := db.Model(&Text{}).First(&text, "id=?", id).Error
+	err := db.Model(&Text{}).Select("texts.id", "texts.user_id", "texts.body", "texts.title", "texts.created_at", "texts.expire_at", "texts.ip_address", "texts.user_country", "texts.text_length", "texts.tag", "texts.language", "user_infos.name", "user_infos.email", "user_infos.picture").Joins("left join user_infos on texts.user_id = user_infos.id").Scan(&TextView{}).First(&text, "texts.id=?", id).Error
+
+	// err := db.Model(&Text{}).First(&text, "id=?", id).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil
 	} else if err != nil {
