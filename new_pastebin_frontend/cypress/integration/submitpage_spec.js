93e059e7a11e025b07d5b4382494022343aed40e
unknown <bkojusner@ufl.edu>
2022-04-18 12:06:35 -0400

    #357 Shell Code for Submitpage Tests

---
+++ new_pastebin_frontend/cypress/integration/submitpage_spec.js
@@ -0,0 +1,17 @@
+describe('General Tests for Submitpage Component', function () {
+    it('Checks for Null Values', function () {
+        // Try to create a paste with an empy Title and Body
+    })
+
+    it('Checks Proper Date', function () {
+        // Will select an old date
+    })
+
+    it('Checks Unlisted Paste Validity', function () {
+        // Creates Unlisted paste and searches it in the homepage
+    })
+
+    it('Selects Different Syntax Highlighting', function () {
+        // Will scroll through different highlighting options
+    })
+})
