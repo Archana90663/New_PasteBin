5c2aca3d5fe0cbba0497ce8d9ab022db856cf5e2
unknown <bkojusner@ufl.edu>
2022-04-18 13:04:01 -0400

    #360 Test Shell Code

---
+++ new_pastebin_frontend/cypress/integration/textpage_spec.js
@@ -0,0 +1,11 @@
+describe('General Tests for Textpage Component', function () {
+    it('Checks Paste Data Integrity', function () {
+        // Create a paste and see if it is the same in the Homepage
+
+        // Checks for it in the homepage
+    })
+
+    it('Checks Text Page Has Proper Info', function () {
+        // Visit Paste Page
+    })
+})
