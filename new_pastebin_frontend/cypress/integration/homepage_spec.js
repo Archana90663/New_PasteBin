234e3d42e31b8b880f8abd98756885e1df532205
unknown <bkojusner@ufl.edu>
2022-04-18 11:31:26 -0400

    #356 Null Values Test Done

--- new_pastebin_frontend/cypress/integration/homepage_spec.js
+++ new_pastebin_frontend/cypress/integration/homepage_spec.js
@@ -1,6 +1,17 @@
 describe('General Tests for Homepage Component', function () {
     it('Checks for Null Values', function () {
         // Try to create a paste with an empy body
+        cy.visit('http://localhost:4200/submitpage')
+        cy.get('#mat-input-0').type('Empty Body Public Paste')
+        cy.get('#mat-input-1').type('2022-06-03T08:30')
+        cy.get('#mat-select-0').click()
+        cy.contains('python').click()
+        cy.get('#mat-select-value-3').click()
+        cy.contains('Public').click()
+        cy.get('#submitButton').click()
+
+        // Alert indicating Null body is not acceptable
+        cy.contains("Cannot post text with empty body")
     })
 
     it('Check User Login Status', function () {
