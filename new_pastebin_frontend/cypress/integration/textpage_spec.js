f7182b1cf0bf348bbeb659150f6aafc619c51ade
unknown <bkojusner@ufl.edu>
2022-04-18 13:07:53 -0400

    #360 Data Integrity Test Added

--- new_pastebin_frontend/cypress/integration/textpage_spec.js
+++ new_pastebin_frontend/cypress/integration/textpage_spec.js
@@ -1,8 +1,22 @@
 describe('General Tests for Textpage Component', function () {
     it('Checks Paste Data Integrity', function () {
         // Create a paste and see if it is the same in the Homepage
+        cy.visit('http://localhost:4200/submitpage')
+        cy.get('#mat-input-0').type('Text Page Test')
+        cy.get('#mat-input-1').type('2022-06-03T08:30')
+        cy.get('#mat-select-0').click()
+        cy.contains('python').click()
+        cy.get('#mat-select-value-3').click()
+        cy.contains('Public').click()
+        cy.pause() // Added to fill out the body
+        cy.get('#submitButton').click()
 
         // Checks for it in the homepage
+        cy.visit('http://localhost:4200/')
+        cy.get('#searchbar').type("Text Page Test")
+        cy.get('#searchbarsubmit').click()
+        cy.contains("Text Page Test")
+        cy.contains("print('Text Page Test Body')")
     })
 
     it('Checks Text Page Has Proper Info', function () {
