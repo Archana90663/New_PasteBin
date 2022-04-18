fa4abd28422783b5edae05c3990d78340a8a38ca
unknown <bkojusner@ufl.edu>
2022-04-18 11:20:57 -0400

    #356 Created shell for tests

---
+++ new_pastebin_frontend/cypress/integration/homepage_spec.js
@@ -0,0 +1,21 @@
+describe('General Tests for Homepage Component', function () {
+    it('Checks for Null Values', function () {
+        // Try to create a paste with an empy body
+    })
+
+    it('Check User Login Status', function () {
+        // Look at the welcome message in the home page
+    })
+
+    it('Checks Logged out User Functions', function () {
+        // Sees if a logged out user can make a private paste
+    })
+
+    it('Checks Paste UserID is right', function () {
+        // Will create a paste and verify the user in the homepage
+    })
+
+    it('Checks Search Works Properly', function () {
+        // Will search for a paste created in the home page
+    })
+})
